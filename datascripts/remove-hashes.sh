c032a27c50174ac86406158e581a0335395be3e7
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-05-20 09:29:24 +0200

    added datascripts directory: used for preparing simulation results data for use by R scripts.

---
+++ datascripts/remove-hashes.sh
@@ -0,0 +1,2 @@
+sed -i '/^#/ d' *.txt
+# note: this is safe to use repeatedly on files, as is will not change anyhting  other than hash lines.
---
+++ datascripts/remove-iteration-numbers.sh
@@ -0,0 +1,2 @@
+sed -i 's/^\w*\ *//' *.txt
+# note: this must only be run once on each file. it will remove the first word from each line, each time it is run!
