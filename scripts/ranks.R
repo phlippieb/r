c308f02d8018822a348e90f874f1383bd0ceefb0
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-06-16 23:03:37 +0200

    removed unused functions in ranks.R

--- scripts/ranks.R
+++ scripts/ranks.R
@@ -1,101 +1,17 @@
-rankAll <- function() {
-  functions <- c( "ackley", "alpine", "elliptic", "eggholder", 
-                  "goldsteinprice", "griewank", "levy", "michalewicz", 
-                  "quadric", "quartic", "rastrigin", "rosenbrock", 
-                  "salomon", "schwefel1_2", "schwefel2_22", "schwefel2_26", 
-                  "sixhump", "spherical", "step", "zakharov");
-  
+# This is Prof Engelbrecht's rank function.
+# I'm not using it because it uses a file as an input,
+# and I made a version that takes data from R instead,
+# which is much nicer imo.
 
-   algorithms <- c("gbest", "lbest", "vn", "spso", "gcstar", "gcring", "gcvn", "bb", "bbe");
-
-   a1 <- 1;
-   count <- 1;
-   algcount <- 1;
-
-   result.df <- data.frame ( a1=rep("", length(functions)),
-                             a2=rep("", length(functions)),
-                             a3=rep("", length(functions)),
-                             a4=rep("", length(functions)),
-                             a5=rep("", length(functions)),
-                             a6=rep("", length(functions)),
-                             a7=rep("", length(functions)),
-                             a8=rep("", length(functions)),
-                             a9=rep("", length(functions)),
-                             a10=rep("", length(functions)),
-                             a11=rep("", length(functions)),
-                             a12=rep("", length(functions)),
-                             a13=rep("", length(functions)),
-                             a14=rep("", length(functions)),
-                             a15=rep("", length(functions)),
-                             a16=rep("", length(functions)),
-                             a17=rep("", length(functions)),
-                             a18=rep("", length(functions)),
-                             a19=rep("", length(functions)),
-                             a20=rep("", length(functions)),
-                             a21=rep("", length(functions)),
-                             a22=rep("", length(functions)),
-                             a23=rep("", length(functions)),
-                             a24=rep("", length(functions)),
-                             a25=rep("", length(functions)),
-                             a26=rep("", length(functions)),
-                             a27=rep("", length(functions)),
-                             a28=rep("", length(functions)),
-                             a29=rep("", length(functions)),
-                             a30=rep("", length(functions)),
-                             a31=rep("", length(functions)),
-                             a32=rep("", length(functions)),
-                             a33=rep("", length(functions)),
-                             a34=rep("", length(functions)),
-                             a35=rep("", length(functions)),
-                             a36=rep("", length(functions)),
-                             
-                             stringsAsFactors=FALSE);
-   result.function <- c();
-
-   for (f in 1:length(functions)) {
-      result.function <- c();
-      rownames(result.df)[f] <- functions[f];
-      algcount <- 1;
-      for (a1 in 1:(length(algorithms)-1)) {
-         for (a2 in (a1+1):length(algorithms)) {
-            print(paste(count, " doing ", algorithms[a1], ".", algorithms[a2], ".", functions[f], ".txt", sep=""));
-            count <- count + 1;
-            rankList <- rank(paste("./mwu/",
-                        algorithms[a1],
-                        ".",
-                        algorithms[a2],
-                        ".",
-                        functions[f],
-                        ".txt",
-                        sep=""
-                     ));
-
-            colnames(result.df)[algcount] <- paste(algorithms[a1], algorithms[a2], sep=".");
-            algcount <- algcount +1;
-            result.function <- c(result.function, unlist(unname(rankList[2])))
-         }
-      }
-      result.df[f,] <- result.function;
-   }
-   write.table(   result.df,
-                  "mwu-results.txt",
-                  row.names=TRUE,
-                  col.names=TRUE,
-                  quote=FALSE,
-                  sep=" "
-               );
-}
-
-
-rank <- function(source) {
-   x <- scan(source, what=list(perf=0,alg=""))
-   data <- c(x$perf)
-   data 
-   group <- factor(x$alg)
-   group
-   rankList <- rankF(data,group,0.05,FALSE)
-   return (rankList)
-}
+#rank <- function(source) {
+#   x <- scan(source, what=list(perf=0,alg=""))
+#   data <- c(x$perf)
+#   data 
+#   group <- factor(x$alg)
+#   group
+#   rankList <- rankF(data,group,0.05,FALSE)
+#   return (rankList)
+#}
 
 rankF <- function(data, group, alpha=0.05, max=TRUE) {
    #initializes the ranks to 0
