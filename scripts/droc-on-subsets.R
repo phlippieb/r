54f1576384f2b7c8a627a7764b1d4b8593d01f60
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-06-17 15:44:36 +0200

    added droc-on-subsets.R for plotting sl1 and sl2 values for different cut-off iteration points. Also some clean-up of R project files.

---
+++ scripts/droc-on-subsets.R
@@ -0,0 +1,42 @@
+# uses pwla.R::pwla.subset(d, i), ::pwla.slope1(df), and ::pwla.slope2(df)
+# plots the slope1 and slope2 values (y-axis) calculated on the data up to different values from iterations (x-axis)
+#
+# param data:		the diversity data; not averaged
+# param iterations:	a c()llection of iterations to consider as the cut-off points for each calculation
+#					tip: can be generated with, for example, seq(from=1, to=1001, at=10)
+plot.droc.foriterations <- function(data, iterations) {
+	# first, average the data:
+	data <- rowMeans(data);
+
+	# calculate sl1 and sl2 for each cut-off iteration:
+	sl1s <- c();
+	sl2s <- c();
+	for (i in iterations) {
+		p <- pwla.subset(data, i);
+		sl1s <- c(sl1s, pwla.slope1(p));
+		sl2s <- c(sl2s, pwla.slope2(p));
+	}
+
+	# plot all sl1 and sl2 values against cut-off iterations
+	#	find xlim and ylim:
+	plot.xmin <- min(iterations);
+	plot.xmax <- max(iterations);
+	plot.ymin <- min(sl1s, sl2s);
+	plot.ymax <- max(sl1s, sl2s);
+	#	plot sl1 lines:
+	plot.points <- matrix(c(iterations, sl1s), ncol=2);
+	colnames(plot.points) <- c("x", "y");
+	plot(	plot.points,
+			xlim=c(plot.xmin, plot.xmax),
+			ylim=c(plot.ymin, plot.ymax),
+			type='l',
+			lty=1
+			)
+	#	plot sl2 lines:
+	plot.points <- matrix(c(iterations, sl2s), ncol=2);
+	colnames(plot.points) <- c("x", "y");
+	lines(	plot.points,
+			xlim=c(plot.xmin, plot.xmax),
+			ylim=c(plot.ymin, plot.ymax),
+			lty=2)
+}
