e8ee9bb6473c3e12d2a763644db22937b7f3819c
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-08-29 11:50:02 +0200

    bug fix

--- scripts/runmwuonpartial.java
+++ scripts/runmwuonpartial.java
@@ -1,3 +1,12 @@
+/*
+
+Note:
+This only prints all the cat commands that are needed to stitch partial mwu together.
+Take the output of this class and run it from the r base dir
+
+*/
+
+
 class runmwuonpartial {
 
 	public static void main (String [] args) {
@@ -5,7 +14,7 @@ class runmwuonpartial {
 		String [] functions = {"ackley", "elliptic", "rastrigin", "rosenbrock", "schwefel1_2", "alpine", "eggholder", "griewank", "levy", "michalewicz", "quadric", "quartic", "salomon", "schwefel2_22","schwefel2_26", "spherical", "step", "zakharov"};
 		String [] algorithms = {"gbest", "lbest", "vn", "spso", "gbestgc", "lbestgc", "vngc", "bb", "bba"};
 
-		for (int a1=0; a1<algorithms.length; a1++) {
+		for (int a1=0; a1<algorithms.length-1; a1++) {
 			for (int a2=a1+1; a2<algorithms.length; a2++) {
 				for (int f=0; f<functions.length; f++) {
 					System.out.println("cat mwu-partial/" + algorithms[a1] + "." + functions[f] + ".partial.txt mwu-partial/" + algorithms[a2] + "." + functions[f] + ".partial.txt > mwu/" + algorithms[a1] + "." + algorithms[a2] + "." + functions[f] + ".txt");
