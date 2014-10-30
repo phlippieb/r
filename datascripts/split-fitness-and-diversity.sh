19e829407dbd4c3ff7ba5f802d4ec745c4f9b02a
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-10-30 16:26:41 +0200

    datascripts changed to only generate diversity data

--- datascripts/split-fitness-and-diversity.sh
+++ datascripts/split-fitness-and-diversity.sh
@@ -2,4 +2,6 @@ for FILE1 in $(ls *.txt)
 do
 	./print-first-30.sh $FILE1 >$FILE1.fit.rdata
 	./print-last-30.sh  $FILE1 >$FILE1.div.rdata
+	# remove fitness: (we're not using it)
+	rm $FILE1.fit.rdata
 done
