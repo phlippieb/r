534b31c2075f08f97cde7fb7d847389bedf0c2f9
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-11-13 16:31:53 +0200

    created scripts for modifying partial data to be distinguishable after stitching together, and for stitching together

--- scripts/stitchpartial.py
+++ scripts/stitchpartial.py
@@ -33,22 +33,21 @@ algorithms = [
 				"vn",
 				"vngc"
 			]
-#iterations = [500, 1000, 5000, 10000]
-iterations = [2000]
+iterations = [500, 1000, 2000, 5000, 10000]
+#iterations = [2000]
 
 
-for a1 in range(0, len(algorithms)):
-	for a2 in range(a1+1, len(algorithms)):
-		for f in range(0, len(functions)):
-			for i in range(0, len(iterations)):
-				print "echo %s // %s // %s // %s" %(algorithms[a1], algorithms[a2], functions[f], iterations[i])
-				print "cat mwu-partial/%s.%s.%s.partial.txt mwu-partial/%s.%s.%s.partial.txt  >  mwu/%s.%s.%s.%s.txt" %(algorithms[a1], 
-																														functions[f], 
-																														iterations[i], 
-																														algorithms[a2], 
-																														functions[f], 
-																														iterations[i], 
-																														algorithms[a1], 
-																														algorithms[a2], 
-																														functions[f], 
-																														iterations[i])
+for a in range(0, len(algorithms)):
+	for f in range(0, len(functions)):
+		for i in range(0, len(iterations)-1):
+			print "echo %s // %s // %s // %s" %(algorithms[a], functions[f], iterations[i], iterations[i+1])
+			print "cat mwu-partial/%s.%s.%s.partial.txt mwu-partial/%s.%s.%s.partial.txt  >  mwu/%s.%s.%s.%s.txt" %(algorithms[a], 
+																													functions[f], 
+																													iterations[i], 
+																													algorithms[a], 
+																													functions[f], 
+																													iterations[i+1], 
+																													algorithms[a], 
+																													functions[f], 
+																													iterations[i],
+																													iterations[i+1])
