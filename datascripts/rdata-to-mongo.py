63c767879e9f8e284a1f78ac53063ec368c5e7ff
Phlippie Bosman <phlippie.bosman@gmail.com>
2015-03-30 18:42:04 +0200

    rdata-to-mongo should now create separate docs for each data item

--- datascripts/rdata-to-mongo.py
+++ datascripts/rdata-to-mongo.py
@@ -53,14 +53,8 @@ for a in range(0, len(algorithms)):
 	for p in range(0, len(populations)):
 		for f in range(0, len(functions)):
 			for d in range(0, len(dimensions)):
-				# print "### running for %s.%s.%s.%s" %(algorithms[a], populations[p], functions[f], dimensions[d])
-				print "db.simulations.insert( {"
-				print "   algorithm: \"%s\"," 	%(algorithms[a])
-				print "   population: \"%s\"," 	%(populations[p])
-				print "   function: \"%s\"," 	%(functions[f])
-				print "   dimension: \"%s\"," 	%(dimensions[d])
-				# read measurement data:
-				print "   measurements: ["
+
+				# diversity data:
 				divfilename = "rdata/" + algorithms[a] + "." + str(populations[p]) + "." + functions[f] + "." + str(dimensions[d]) + ".div"
 				f = open(divfilename)
 				f_byrow = []
@@ -68,23 +62,35 @@ for a in range(0, len(algorithms)):
 					f_byrow.append(line.split())
 				f_bycol = zip(*f_byrow)
 				for sample in range(0, len(f_bycol)):
-					print "      {" #begin sample document
-					print "        sample: \"%s\"," %(sample)
-					print "        data: ["
 					for iteration in range (0, len(f_byrow)):
-						print "         {" #begin iteration document
-						print "           iteration: \"%s\"," %(iteration)
-						print "           diversity: \"%s\"" %(f_bycol[sample][iteration])
-						if iteration < (len(f_byrow)-1):
-							print "         }," #close iteration document
-						else:
-							print "         }" #close last iteration document
-					print "        ]"
-					if sample < (len(f_bycol)-1):
-						print "      }," #close sample document
-					else:
-						print "      }"	#close last sample document
-				print "   ]"
+						print "db.simulations.insert({"
+						print "   algorithm: \"%s\"," 	%(algorithms[a])
+						print "   population: \"%s\"," 	%(populations[p])
+						print "   function: \"%s\"," 	%(functions[f])
+						print "   dimension: \"%s\"," 	%(dimensions[d])
+						print "   sample: \"%s\"," 		%(sample)
+						print "   iteration: \"%s\"," 	%(iteration)
+						print "   diversity: \"%s\"" 	%(f_bycol[sample][iteration])
 				print "})"
-                                print ""
+				f.close()
 
+				# fitness data:
+				fitfilename = "rdata/" + algorithms[a] + "." + str(populations[p]) + "." + functions[f] + "." + str(dimensions[d]) + ".fit"
+				f = open(fitfilename)
+				f_byrow = []
+				for line in f:
+					f_byrow.append(line.split())
+				f_bycol = zip(*f_byrow)
+				for sample in range(0, len(f_bycol)):
+					for iteration in range (0, len(f_byrow)):
+						print "db.simulations.insert({"
+						print "   algorithm: \"%s\"," 	%(algorithms[a])
+						print "   population: \"%s\"," 	%(populations[p])
+						print "   function: \"%s\"," 	%(functions[f])
+						print "   dimension: \"%s\"," 	%(dimensions[d])
+						print "   sample: \"%s\"," 		%(sample)
+						print "   iteration: \"%s\"," 	%(iteration)
+						print "   fitness: \"%s\"" 	%(f_bycol[sample][iteration])
+				print "})"
+				print ""
+				f.close()
