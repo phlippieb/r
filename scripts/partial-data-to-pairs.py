8b6c1bd1a86bf6051121ae30987a4160c7ca488f
Phlippie Bosman <phlippie.bosman@gmail.com>
2015-04-21 13:15:13 +0200

    Gave stitching script progress counting; supports resolutions

--- scripts/partial-data-to-pairs.py
+++ scripts/partial-data-to-pairs.py
@@ -55,14 +55,25 @@ resolutions = [
 				10
 ]
 
+length = {
+	'algorithms': len(algorithms),
+	'functions': len(functions),
+	'iterations': len(iterations),
+	'resolutions': len(resolutions)
+}
 
-for a1 in range(0, len(algorithms)):
+
+currentrun = 1
+totalruns = ((length['algorithms']-1) * (length['algorithms']) / 2) * length['functions'] * length['iterations'] * length['resolutions']
+
+for a1 in range(0, len(algorithms)-1):
 	for a2 in range(a1+1, len(algorithms)):
 		for f in range(0, len(functions)):
 			for i in range(0, len(iterations)):
 				for r in range(0, len(resolutions)):
-					print "echo %s // %s // %s // %s // %s" %(algorithms[a1], algorithms[a2], functions[f], iterations[i], resolutions[r])
-					print "cat drocdata/%s.%s.%sr.%si.droc.txt drocdata/%s.%s.%sr.%si.droc.txt  >  mwu/%s.%s.%s.%sr.%si.txt" %(algorithms[a1], 
+					print "echo [%s/%s] %s // %s // %s // %s // %s" %(currentrun, totalruns, algorithms[a1], algorithms[a2], functions[f], iterations[i], resolutions[r])
+					currentrun += 1
+					print "cat drocdata/%s.%s.%sr.%si.droc.txt drocdata/%s.%s.%sr.%si.droc.txt  >  drocpairs/%s.%s.%s.%sr.%si.txt" %(algorithms[a1], 
 																															functions[f],
 																															resolutions[r],
 																															iterations[i], 
@@ -75,3 +86,4 @@ for a1 in range(0, len(algorithms)):
 																															functions[f], 
 																															resolutions[r],
 																															iterations[i])
+#print "echo total runs required was %s a1 \* %s a2 \* %s f \* %s i \* %s r = %s" %((len(algorithms)-1), (len(algorithms)-1), len(functions), len(iterations), len(resolutions), totalruns)
