d0af939d45812adce18cec28e7cb64e158f7cf6b
Phlippie Bosman <phlippie.bosman@gmail.com>
2015-06-01 23:00:09 +0200

    added scripts to mwu-compare droc for algorithms at several resolutions

--- scripts/pair-droc-by-algorithm.py
+++ scripts/pair-droc-by-algorithm.py
@@ -1,89 +1,91 @@
-# NOTE:
-# Not sure what this does with missing functions (at this point, the 2 fixed-dimension ones are Goldstein-Price and Six-Hump Camel Back)
+# Usage note:
+# This script produces bash commands that will stitch pairs of droc-data files together.
+# A one-liner for this is something like:
+#   python <this.py> | bash
 
-functions = [	"ackley",
-				"elliptic", 
-				"rastrigin", 
-				"rosenbrock", 
-				"schwefel1_2", 
-				"alpine", 
-				"eggholder", 
-				"griewank", 
-				"levy", 
-				"michalewicz", 
-				"quadric", 
-				"quartic", 
-				"salomon", 
-				"schwefel2_22",
-				"schwefel2_26", 
-				"spherical", 
-				"step", 
-				"zakharov", 
-				"goldsteinprice", 
-				"sixhump"
-			]
+# This script can be used to create paired-up files, each containing the droc-data of two simulations, where the two simulations are identically defined in terms of algorithm, population, function, dimensions, and iterations, but differ in their resolution.
+# This requires files to be in a two-column format, where the first column is the droc value of a specific simulation (with multiple rows for multiple samples), and the second column contains the name of the resolution used.
 
-functions = [
-				"goldsteinprice",
-				"sixhump"
-			]
+# should we compare every resolution to every other (rn/rm; n!=m), or just to every adjacent resolution (rn/rn+1)?
+# (all to all is /pretty/ intense yo)
+allToall = True
+
+functions = [	
+	"ackley",
+	"elliptic", 
+	"rastrigin", 
+	"rosenbrock", 
+	"schwefel1_2", 
+	"alpine", 
+	"eggholder", 
+	"goldsteinprice",
+	"griewank", 
+	"levy", 
+	"michalewicz", 
+	"quadric", 
+	"quartic", 
+	"salomon", 
+	"schwefel2_22",
+	"schwefel2_26", 
+	"sixhump",
+	"spherical", 
+	"step", 
+	"zakharov",
+]
 			
 algorithms = [
-				"bb",
-				"bba",
-				"gbest",
-				"gbestgc",
-				"lbest",
-				"lbestgc",
-				"spso",
-				"vn",
-				"vngc"
-			]
-
-iterations = [2000]
-
-resolutions = [
-				1,
-				2,
-				3,
-				4,
-				5,
-				6,
-				7,
-				8,
-				9,
-				10
+	"bb",
+	"bba",
+	"gbest",
+	"gbestgc",
+	"lbest",
+	"lbestgc",
+	"spso",
+	"vn",
+	"vngc"
 ]
 
-length = {
-	'algorithms': len(algorithms),
-	'functions': len(functions),
-	'iterations': len(iterations),
-	'resolutions': len(resolutions)
-}
+iterations = [
+	2000
+]
 
+resolutions = range(1,20)
 
-currentrun = 1
-totalruns = ((length['algorithms']-1) * (length['algorithms']) / 2) * length['functions'] * length['iterations'] * length['resolutions']
+# create the drocpairs directory if necessary
+import os
+algpairs = os.path.dirname("drocpairs/alg/x.txt")
+if not os.path.exists(algpairs):
+	os.makedirs(algpairs)
 
-for a1 in range(0, len(algorithms)-1):
-	for a2 in range(a1+1, len(algorithms)):
-		for f in range(0, len(functions)):
-			for i in range(0, len(iterations)):
+if not allToall:
+	print "this script isn't implemented for this type of operation (see code re: allToAll)"
+#	currentrun = 1
+#	totalruns = len(algorithms) * len(functions) * len(iterations) * (len(resolutions))
+#	for a in range (0, len(algorithms)):
+#		for f in range(0, len(functions)):
+#			for r in range(0, len(resolutions)):
+#				for i in range(0, len(iterations)):
+#					print "echo -ne \"\r[%s/%s] %s %s %s %s %s                \"" %(currentrun, totalruns, algorithms[a], functions[f], resolutions[r], resolutions[r]+1, iterations[i])
+#					currentrun += 1
+#					print "cat drocdata/%s.%s.%sr.%si.droc.txt drocdata/%s.%s.%sr.%si.droc.txt > drocpairs/res/%s.%s.%sr.%sr.%si.txt" %(
+#						algorithms[a], functions[f], resolutions[r], iterations[i], 
+#						algorithms[a], functions[f], resolutions[r], resolutions[r]+1, iterations[i]
+#					)
+else:
+	currentrun = 1
+	import math
+	totalruns = len(resolutions) * len(functions) * len(iterations) * len(algorithms)
+	for a1 in range (0, len(algorithms)-1):
+		for a2 in range (a1+1, len(algorithms)):
+			for f in range(0, len(functions)):
 				for r in range(0, len(resolutions)):
-					print "echo [%s/%s] %s // %s // %s // %s // %s" %(currentrun, totalruns, algorithms[a1], algorithms[a2], functions[f], iterations[i], resolutions[r])
-					currentrun += 1
-					print "cat drocdata/%s.%s.%sr.%si.droc.txt drocdata/%s.%s.%sr.%si.droc.txt  >  drocpairs/%s.%s.%s.%sr.%si.txt" %(algorithms[a1], 
-																															functions[f],
-																															resolutions[r],
-																															iterations[i], 
-																															algorithms[a2], 
-																															functions[f], 
-																															resolutions[r], 
-																															iterations[i],
-																															algorithms[a1], 
-																															algorithms[a2], 
-																															functions[f], 
-																															resolutions[r],
-																															iterations[i])
-#print "echo total runs required was %s a1 \* %s a2 \* %s f \* %s i \* %s r = %s" %((len(algorithms)-1), (len(algorithms)-1), len(functions), len(iterations), len(resolutions), totalruns)
+					for i in range(0, len(iterations)):
+						print "echo -ne \r[%s/???] %s %s %s %s %s                " %(currentrun, algorithms[a1], algorithms[a2], functions[f], resolutions[r], iterations[i])
+						currentrun += 1
+						print "cat drocdata/%s.%s.%sr.%si.droc.txt drocdata/%s.%s.%sr.%si.droc.txt > drocpairs/alg/%s.%s.%s.%sr.%si.txt" %(
+							algorithms[a1], functions[f], resolutions[r], iterations[i], 
+							algorithms[a2], functions[f], resolutions[r], iterations[i], 
+							algorithms[a1], algorithms[a2], functions[f], resolutions[r], iterations[i]
+						)
+print "echo"
+
---
+++ scripts/rank-all-algpairs-resolutions.R
@@ -0,0 +1,84 @@
+rank.all.algs.resolutions <- function() {
+
+   resolutions <- 1:20;
+
+   functions <- c(   
+      "ackley",
+      "alpine",
+      "eggholder",
+      "elliptic",
+      "goldsteinprice",
+      "griewank",
+      "levy",
+      "michalewicz",
+      "quadric",
+      "quartic",
+      "rastrigin",
+      "rosenbrock",
+      "salomon",
+      "schwefel1_2",
+      "schwefel2_22",
+      "schwefel2_26",
+      "sixhump",
+      "spherical",
+      "step",
+      "zakharov"
+   );
+
+   algorithms <- c(  
+      "bb",
+      "bba",
+      "gbest",
+      "gbestgc",
+      "lbest",
+      "lbestgc",
+      "spso",
+      "vn",
+      "vngc"
+   );
+
+   algorithmpairs <- c();
+   for (i in  1:length(algorithms)-1) {
+      for (j in 1:length(algorithms)) {
+         if (j > i+1) {
+            algorithmpairs <- c(
+               algorithmpairs,
+               paste(algorithms[i+1], algorithms[j], sep="."));
+         }
+      }
+   }
+
+   print (algorithmpairs); Sys.sleep(3);
+
+   result.m <- matrix(
+      nrow=length(algorithmpairs),
+      ncol=length(resolutions)
+   );
+
+   for (aa in 2:length(algorithmpairs)) {
+      for (r in 1:length(resolutions)) {
+         result.m [aa,r] <- 0;
+         mwu.sum <- 0;
+         for (f in 1:length(functions)) {
+            filename = paste(
+               "./drocpairs/alg/",
+               algorithmpairs[aa], ".",
+               functions[f], ".",
+               resolutions[r], "r.",
+               "2000i.txt",
+               sep=""
+            );
+            if (file.exists(filename)) {
+               rankresult = rank(filename);
+               mwu.sum = mwu.sum + rankresult;
+            } else {
+               cat(paste("\rFILE NOT FOUND:  ", filename, "\n", sep=""));
+               #Sys.sleep(1);
+            }
+         }
+         result.m[aa,r] <- mwu.sum; # this can potentially be cleaned up to not use mwu.sum
+      }
+   }
+
+   return (result.m);
+}
