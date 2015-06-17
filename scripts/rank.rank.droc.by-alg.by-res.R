89640d3b67e5ca1085b1574440098ba97b9db4cf
Phlippie Bosman <phlippie.bosman@gmail.com>
2015-06-17 14:50:57 +0200

    incomplete implementation to rank ranks

--- scripts/rank.rank.droc.by-alg.by-res.R
+++ scripts/rank.rank.droc.by-alg.by-res.R
@@ -5,7 +5,7 @@
 # http://stackoverflow.com/questions/3968281/git-pushing-amended-commits
 # (om met onvolledige commits te werk)
 rank.comparisons.byres <- function() {
-	resolutions <- 1:10
+	resolutions <- 1:2
 	
 	functions <- c(   
 		"ackley",
@@ -64,18 +64,13 @@ rank.comparisons.byres <- function() {
 
 	# for each resolution:
 	for (r in 1:length(resolutions)) {
-		print(paste("RES = ", resolutions[r], sep=""));
 		thisRow <- c();
 
 		# for each algorithm pair:
 		for (aa in 2:length(algorithmpairs)) {
 
-			print(paste("AA = ", algorithmpairs[aa], sep=""));
-
 			# for each function:
 			for (f in 1:length(functions)) {
-
-				print(paste("F = ", functions[f], sep=""));
 
 				# get the file with the data
 				# (paired up by algorithm)
@@ -89,14 +84,11 @@ rank.comparisons.byres <- function() {
 				);
 				if (file.exists(filename)) {
 					# calculate rank on algorithm pair for this function and resolution
-					rankresult = unlist(rank(filename))[1];
-
-					print(paste("RESULT = ", rankresult, sep=""));
+					rankresult = unlist(rank.file(filename))[1];
 
 					# append to results for this resolution
 					thisRow <- c(thisRow, rankresult);
 
-					print("THISROW = "); print (thisRow);
 				} else {
 					# if file is not found, stop executing with an error message
 					stop(paste("\rFILE NOT FOUND:  ", filename, "\n", sep=""));
@@ -111,8 +103,6 @@ rank.comparisons.byres <- function() {
 		allResults.rowNames <- c(allResults.rowNames, r);
 	}
 
-	print(allResults);
-
 	# name rows in allResults to reflect the resolutions used
 	rownames(allResults) <- allResults.rowNames;
 
@@ -125,8 +115,25 @@ rank.comparisons.byres <- function() {
 	}
 	colnames(allResults) <- allResults.colNames;
 
-	return (allResults);
+	return (allResults)
 
-	# TODO: rank the results!
+	# rank the ranks!
+
+	#finalResults <- c();
+#
+#	#for (r in 1:(length(resolutions)-1)) {
+#	#	print("trying to rank two rows...")
+#	#	print("these are the two rows:")
+#	#	print(allResults[r])
+#	#	print(allResults[r+1])
+#	#	x <- rank(c(allResults[r], allResults[r+1]), c(r, r+1));
+#	#	print("done! getting results...")
+#	#	x <- unlist(unname(x[1]));
+#	#	print("done! pasting results together...")
+#	#	finalResults <- c(finalResults, x);
+#	#}
+#
+#	#print("all done! returning results.")
+	#return (finalResults);
 
 }
